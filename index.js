4685ae6f560a60707b7c582c3cbe076a53fb5a95
lauren kernan <lauren.kernan17@gmail.com>
2019-01-07 19:55:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ document.getElementById("Titanic").addEventListener("click", (e) => {
   let content = (movies.Titanic.reviews[i].content);
   
   var reviewsTi = document.createElement('p');
-  reviewsTi.innerHTML = username+ " " +content;
+  reviewsTi.innerHTML = username+ "<br>" +content;
   document.getElementById("reviews").appendChild(reviewsTi);
   
   
